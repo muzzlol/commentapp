e63b82b28d709056ed1a15150a6c3c32d1dc9b63
muzzlol <muzxmmilkhxn@gmail.com>
2025-07-04 00:47:13 +0530

    fe init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="fixed inset-0 flex justify-center items-center">
+      <h1 className="text-3xl font-semibold">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
