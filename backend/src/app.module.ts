497cd63697601719b2de890e52924d21f1c556af
muzzlol <muzxmmilkhxn@gmail.com>
2025-07-08 01:10:14 +0530

    fix for BE deploymenttt

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
@@ -9,6 +10,9 @@ import { NotificationModule } from './notification/notification.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     PrismaModule, 
     AuthModule, 
     CommentModule, 
