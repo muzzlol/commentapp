56df59d8969d6757ba2ab4d0247ee4eca117d0f7
muzzlol <muzxmmilkhxn@gmail.com>
2025-07-07 22:20:09 +0530

    prepared .env setup for uploading to sanctity application

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
@@ -8,7 +9,16 @@ import { CommentModule } from './comment/comment.module';
 import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, CommentModule, NotificationModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.dev',
+      isGlobal: true,
+    }),
+    PrismaModule, 
+    AuthModule, 
+    CommentModule, 
+    NotificationModule
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
